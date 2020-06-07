6d27f8f202d4f205587561a64314587e0694734d
Abhinav Singh <abhinav.s0612@gmail.com>
2020-06-07 20:36:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cinewatch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cinewatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
