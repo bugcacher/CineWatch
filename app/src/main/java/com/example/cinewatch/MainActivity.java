5232889538d888d59f3750cf2b922c3c039d4e2b
Abhinav Singh <abhinav.s0612@gmail.com>
2020-06-09 02:07:18 +0530

    added layout files

--- app/src/main/java/com/example/cinewatch/MainActivity.java
+++ app/src/main/java/com/example/cinewatch/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.cinewatch;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.graphics.Outline;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.movie_details);
+
     }
 }
